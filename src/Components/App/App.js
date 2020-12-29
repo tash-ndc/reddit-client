3a16209a7fdeb5745790ae237b6666cb93b1ccca
NDC <74566670+tash-ndc@users.noreply.github.com>
2020-12-29 16:41:28 +0000

    minor settings and nav component done

---
+++ src/Components/App/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import Nav from '../Nav/Nav';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="container">
+                <Nav />
+            </div>
+        )
+    }
+
+};
+
+export default App;
